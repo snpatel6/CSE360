c507484f3094fd569363d9bf0fcda56443350677
shivp <shivp@shivs-mbp.3521.dhcp.asu.edu>
2020-02-27 16:05:07 -0700

    final version

--- Assignment2/src/cse360assign2/SimpleList.java
+++ Assignment2/src/cse360assign2/SimpleList.java
@@ -15,7 +15,7 @@ public class SimpleList {
 	public SimpleList() 
 	{
 		this.count = 0;
-		list = new int[11];
+		list = new int[10];
 	}
 	
 /**
@@ -24,6 +24,8 @@ public class SimpleList {
  * If the number of elements in array represented by count is between 1 and 9, then element  
  * at index of count will move to index count + 1 for all elements.
  * If count is 10, the last element would be replaced by one before for all elements then first element is set to input.
+ * If the count is 10, I have made a temporary list which stores all the information from the old list. Then I increase size
+ * of the old list and then move everything back into the old list from temp. 
  * 
  * @param input		element user wants to add.
  */
@@ -34,9 +36,9 @@ public class SimpleList {
 	           list[count] = input;
 	           count++;
 	       } 
-	       else if (count < 10) 
+	       else if (count < list.length) 
 	       {
-	           for (int i = count; i >= 0; i--) 
+	           for (int i = count - 1; i >= 0; i--) 
 	           {
 	               list[i + 1] = list[i];
 	           }
@@ -45,11 +47,26 @@ public class SimpleList {
 	       } 
 	       else 
 	       {
-	           for (int i = count; i > 0; i--) 
-	           {
-	               list[i] = list[i - 1];
+	           int temp[] = new int[list.length];
+	           for(int i = 0; i < list.length; i++) {
+	        	   temp[i] = list[i];
 	           }
+	            
+	           int k = (int) (list.length*(50.0f/100.0f));
+	           list = new int[list.length+k];
+	           
+	           
+	           for(int i = 0; i < temp.length; i++) {
+	        	   list[i] = temp[i];
+	           }
+	           
+	           for (int i = count - 1; i >= 0; i--) 
+	           {
+	               list[i + 1] = list[i];
+	           }
+	           
 	           list[0] = input;
+	           count++;
 	       }
 	 }
 	
@@ -57,6 +74,8 @@ public class SimpleList {
  * remove(int) function will use the search function to find the index of where input is located.
  * If the index is found, the element after that index will be placed where the input is for all elements till last.
  * Then I set the last element to 0 and decrement the counter.
+ * I would always check how many empty spots are there and If 25% or more empty spots exist, i RESIZE ARRAY TO 
+ * SIZE OF NUMBER OF ELEMENTS THAT ARE IN THE LIST. 
  * 
  * @param input		element user want to remove.
  */
@@ -65,12 +84,26 @@ public class SimpleList {
 	       int index = search(input);
 	       if (index != -1) 
 	       {
-	           for (int i = index; i < count - 1; i++) 
+	           for (int i = index; i < count; i++) 
 	           {
-	               list[i] = list[i + 1];
+	               if((i+1) != list.length) {
+	            	   list[i] = list[i + 1];
+	               }
 	           }
-	           list[count] = 0;
-	           count--;           
+	           list[count - 1] = 0;
+	           count--;    
+	           int emptyspots = list.length - count;
+	           int k = (int) (list.length*(25.0f/100.0f));
+	           if(emptyspots >= k && count != 1) {
+	        	   int temp[] = new int[count];
+	        	   for(int i = 0; i < count; i++) {
+	        		   temp[i] = list[i];
+	        	   }
+	        	   list = new int[count];
+	        	   for(int i = 0; i < temp.length; i++) {
+	        		   list[i] = temp[i];
+	        	   }
+	           }	           	           
 	       } 
 	 }
 	
@@ -85,6 +118,12 @@ public class SimpleList {
 	}
 	
 	
+/**
+ * search() method takes a input and traverses though the list and if any matches, it returns the index at that spot. 
+ * 
+ * @param input
+ * @return index	returns the index of where that element is stored.
+ */
 	public int search(int input) 
 	{
 	       int index = -1;
@@ -96,6 +135,59 @@ public class SimpleList {
 	           }
 	       }
 	       return index;
+	}
+	
+/**
+ * The append function checks if the length is larger than the count to see how many elements are there. 
+ * If the size of array and count is same, I would add the element and just like add function after
+ * I increase the size of array by 50%. If count is less than length, then I just add like normal without changing array size.
+ * @param input
+ */
+	public void append(int input) {
+		if(list.length > count) {
+			list[count] = input;
+			count++;
+		} else {
+		    int temp[] = new int[list.length];
+            
+		    for(int i = 0; i < list.length; i++) {
+        	   temp[i] = list[i];
+            }
+	            
+	        int k = (int) (list.length*(50.0f/100.0f));
+	        list = new int[list.length+k];
+	              
+            for(int i = 0; i < temp.length; i++) {
+        	   list[i] = temp[i];
+            }
+            
+        	list[count] = input;
+   			count++;
+		}
+	}
+	
+/**
+ * first() method returns the first element in the list or if it is empty it returns -1
+ * @return integer	returns the first element or -1 if list is empty
+ */
+	public int first() {
+		if(count != 0) {
+			return list[0];
+		} else {
+			return -1;
+		}
+	}
+
+/**
+ * last() method returns the last element in the list or if it is empty it returns -1
+ * @return integer	returns the last element or -1 if list is empty
+ */
+	public int last() {
+		if(count != 0) {
+			return list[count-1];
+		} else {
+			return -1;
+		}
 	}
 	
 /**
